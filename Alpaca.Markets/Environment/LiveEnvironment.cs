7cbc054582aa673d7804f356f620d1794b2ad8a5
sghitch <sghitch@stanford.edu>
2020-08-27 18:16:25 -0700

    Update environment URLs

--- Alpaca.Markets/Environment/LiveEnvironment.cs
+++ Alpaca.Markets/Environment/LiveEnvironment.cs
@@ -4,16 +4,16 @@ namespace Alpaca.Markets
 {
     internal sealed class LiveEnvironment : IEnvironment
     {
-        public Uri AlpacaTradingApi { get; } = new Uri("https://api.alpaca.markets");
+        public Uri AlpacaTradingApi { get; } = new Uri(@"https://api.alpaca.markets");
 
-        public Uri AlpacaDataApi { get; } = new Uri("https://data.alpaca.markets");
+        public Uri AlpacaDataApi { get; } = new Uri(@"https://data.alpaca.markets");
 
-        public Uri PolygonDataApi { get; } = new Uri("https://api.polygon.io");
+        public Uri PolygonDataApi { get; } = new Uri(@"https://api.polygon.io");
 
-        public Uri AlpacaStreamingApi { get; } = new Uri("wss://api.alpaca.markets/stream");
+        public Uri AlpacaStreamingApi { get; } = new Uri(@"wss://api.alpaca.markets/stream");
 
-        public Uri PolygonStreamingApi { get; } = new Uri("wss://socket.polygon.io/stocks");
+        public Uri PolygonStreamingApi { get; } = new Uri(@"wss://socket.polygon.io/stocks");
 
-        public Uri AlpacaDataStreamingApi { get; } = new Uri("wss://data.alpaca.markets/stream");
+        public Uri AlpacaDataStreamingApi { get; } = new Uri(@"wss://data.alpaca.markets/stream");
     }
 }
